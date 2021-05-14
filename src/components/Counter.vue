18db5d54b018e8f7a0dab9c9f1cf879377d17ae9
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-05-14 11:22:11 +0100

    count

---
+++ src/components/Counter.vue
@@ -0,0 +1,83 @@
+<template>
+  <div v-if="loaded">
+    <h5 v-if="!expired">Buy Now</h5>
+    <h5 v-else>Timer expired</h5>
+    {{ displayDays }}
+    {{ displayHours }}
+    {{ displayMinutes }}
+    {{ displaySeconds }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["year", "month", "date", "hour", "minute", "second", "millisecond"],
+
+  data: () => ({
+    displayDays: 0,
+    displayHours: 0,
+    displayMinutes: 0,
+    displaySeconds: 0,
+    loaded: false,
+    expired: false,
+  }),
+  computed: {
+    _seconds: () => 1000,
+    _minutes() {
+      return this._seconds * 60;
+    },
+    _hours() {
+      return this._minutes * 60;
+    },
+    _days() {
+      return this._hours * 24;
+    },
+    end() {
+      return new Date(
+        this.year,
+        this.month,
+        this.date,
+        this.hour,
+        this.minute,
+        this.second,
+        this.millisecond
+      );
+    },
+  },
+  mounted() {
+    this.showRemaining();
+  },
+
+  methods: {
+    formatNum: (num) => (num < 10 ? "0" + num : num),
+
+    showRemaining() {
+      const timer = setInterval(() => {
+        const now = new Date();
+        // const end = new Date(2022, 4, 15, 10, 10, 10, 10);
+        const distance = this.end.getTime() - now.getTime();
+
+        if (distance < 0) {
+          clearInterval(timer);
+          this.expired = true;
+          this.loaded = true;
+          return;
+        }
+
+        const days = Math.floor(distance / this._days);
+        const hours = Math.floor((distance % this._days) / this._hours);
+        const minutes = Math.floor((distance % this._hours) / this._minutes);
+        const seconds = Math.floor((distance % this._minutes) / this._seconds);
+
+        this.displayMinutes = this.formatNum(minutes);
+        this.displaySeconds = this.formatNum(seconds);
+        this.displayHours = this.formatNum(hours);
+        this.displayDays = this.formatNum(days);
+        this.loaded = true;
+      }, 1000);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>Hello World</h3>
+    <Counter
+      :year="2022"
+      :month="4"
+      :date="14"
+      :hour="0"
+      :minute="0"
+      :second="0"
+      :millisecond="0"
+    />
+  </div>
+</template>
+
+<script>
+import Counter from "./Counter.vue";
+export default {
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<style scoped></style>
